c761c5959864c131747e67a9b4eb4cac10b111cf
PremAnbu <premanbu9360@gmail.com>
2024-06-05 11:45:46 +0530

    implemented header, login, signup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,42 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { HeaderComponent } from './components/header/header.component';
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
+
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
+import { HttpClientModule } from '@angular/common/http';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { MatDialogModule} from '@angular/material/dialog';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LoginComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatIconModule ,
+    HttpClientModule,
+    MatMenuModule ,
+    MatDividerModule,
+    MatFormFieldModule,
+    MatInputModule ,
+    MatButtonModule,
+    BrowserAnimationsModule,
+    MatDialogModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/services/dataservice.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataserviceService {
+
+  constructor() { }
+  private userRole=new BehaviorSubject('')
+  userRoleState=this.userRole.asObservable();
+
+  private drawerState = new BehaviorSubject(false);
+  currDrawerState = this.drawerState.asObservable();
+
+  changeUserRole(value:string)
+  {
+    this.userRole.next(value);
+  }
+  
+  changeDrawerState(state: boolean) {
+    this.drawerState.next(state)
+  }}
